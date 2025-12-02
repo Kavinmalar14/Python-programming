5bcb514cf8409149c9feeed4c5d4b9deefd89d2e
Kavinmalar14 <parthibanpalaniappan@gmail.com>
2025-12-02 18:11:25 +0000

    Add files via upload

---
+++ Reverse.py
@@ -0,0 +1,4 @@
+a=str(input("Enter any word "))
+reverse=a[::-1]
+print("The reverse of the given string is")
+print(reverse)
---
+++ Type casting.py
@@ -0,0 +1,10 @@
+age=12
+weight=40.5
+print("Before type casting")
+print("Type of age", type(age))
+print("Type of weight", type(weight))
+print("After type casting")
+age=str(age)
+weight=int(weight)
+print("Type of age", type(age))
+print("Type of weight", type(weight))
