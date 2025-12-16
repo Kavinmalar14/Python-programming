ead07c76091e851f2dd3d9d2836cc092f2b43431
Kavinmalar14 <parthibanpalaniappan@gmail.com>
2025-12-16 18:28:11 +0000

    Add files via upload

---
+++ activity 3.py
@@ -0,0 +1,8 @@
+print("enter the marks obtained in four subjects")
+english=int(input("english: "))
+maths=int(input("maths: "))
+drama=int(input("drama: "))
+science=int(input("science: "))
+sum=english+maths+drama+science
+percentage=(sum/400)*100
+print("percentage:", percentage)
