2f9612a6049e6c7fe5b905008449ccf17c995d6d
Kavinmalar14 <parthibanpalaniappan@gmail.com>
2026-03-17 18:48:28 +0000

    Add files via upload

---
+++ reverse order hw.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+
+sum = 0
+
+temp = num
+counter = 0 
+while temp > 0:
+    digit = temp % 10 
+    sum += digit ** 3
+    temp //= 10
+    counter = counter+1
+print(counter)
