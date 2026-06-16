35bae319ca6989f3ec18fafc229f662d608caf86
Kavinmalar14 <parthibanpalaniappan@gmail.com>
2026-06-16 19:46:55 +0100

    Add files via upload

---
+++ symmetric difference.py
@@ -0,0 +1,8 @@
+set1 = {"movies", "books"}
+set2 = {"books", "series"}
+print("Original set elements:")
+print(set1)
+print(set2)
+print("\nSymmetric difference of two said sets:")
+set3 = set1.symmetric_difference(set2)
+print(set3)
